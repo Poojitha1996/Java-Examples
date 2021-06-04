615035dc92420df7135937bf25249aa18349381d
Poojitha1996 <84942766+Poojitha1996@users.noreply.github.com>
2021-06-04 18:02:52 -0400

    Add files via upload

---
+++ Arraylistexample.java
@@ -0,0 +1,14 @@
+
+
+	import java.util.*;  
+	 public class Arraylistexample{  
+	 public static void main(String args[]){  
+	  ArrayList<String> list=new ArrayList<String>();   
+	      list.add("car");   
+	      list.add("truck");    
+	      list.add("cycle");    
+	      list.add("cycle");    
+	    
+	      System.out.println(list);  
+	 }  
+}
---
+++ TestArrayLinked.java
@@ -0,0 +1,23 @@
+
+
+	import java.util.*;    
+	class TestArrayLinked{    
+	 public static void main(String args[]){    
+	     
+	  List<String> al=new ArrayList<String>();  
+	  al.add("choclate"); 
+	  al.add("blueberry");    
+	  al.add("vanilla");    
+	  al.add("choco");    
+	    
+	  List<String> al2=new LinkedList<String>();    
+	  al2.add("pooji");
+	  al2.add("raghava");    
+	  al2.add("Socio");    
+	  al2.add("Socio");    
+	    
+	  System.out.println("arraylist: "+al);  
+	  System.out.println("linkedlist: "+al2);  
+	 }    
+	}    
+
